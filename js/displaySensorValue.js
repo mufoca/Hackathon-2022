93b663faccf4d043f5241abf30e35f839e176206
mufoca <focahackathonteam@gmail.com>
2022-10-07 17:11:23 +0530

    init

--- js/displaySensorValue.js
+++ js/displaySensorValue.js
@@ -1,16 +1,16 @@
 const selectSensor = document.getElementsByClassName("select_sensor");
 
-for (let i = 2; i > 0; i--) {
-    let option = `<option value="sensor${i}">Sensor ${i}: ${i+30}&#8451;</option>`;
+for (let i = 1; i > 0; i--) {
+    let option = `<option value="sensor${i}" id="temp_option${i}">Sensor ${i}: ${i+30}&#8451;</option>`;
     selectSensor[0].firstElementChild.insertAdjacentHTML("afterend", option);
 }
 
-for (let i = 2; i > 0; i--) {
-    let option = `<option value="sensor${i}">Sensor ${i}: ${i+40}</option>`;
+for (let i = 1; i > 0; i--) {
+    let option = `<option value="sensor${i}" id="moisture_option${i}">Sensor ${i}: ${i+40}</option>`;
     selectSensor[1].firstElementChild.insertAdjacentHTML("afterend", option);
 }
 
-for (let i = 2; i > 0; i--) {
-    let option = `<option value="sensor${i}">Sensor ${i}: ${i+800}</option>`;
+for (let i = 1; i > 0; i--) {
+    let option = `<option value="sensor${i}" id="hum_option${i}">Sensor ${i}: ${i+800}</option>`;
     selectSensor[2].firstElementChild.insertAdjacentHTML("afterend", option);
 }
--- js/fetchLiveData.js
+++ js/fetchLiveData.js
@@ -1,40 +1,60 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.9.3/firebase-app.js";
+import {
+  getDatabase,
+  ref,
+  onValue,
+  push,
+  set,
+  update,
+} from "https://www.gstatic.com/firebasejs/9.9.3/firebase-database.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCd-ZXpym75WVSQp9_mTzlTmt2_4ptJtGs",
+  authDomain: "hackathon-2022-81f7b.firebaseapp.com",
+  databaseURL: "https://hackathon-2022-81f7b-default-rtdb.firebaseio.com",
+  projectId: "hackathon-2022-81f7b",
+  storageBucket: "hackathon-2022-81f7b.appspot.com",
+  messagingSenderId: "466349206375",
+  appId: "1:466349206375:web:3fa656e4ebfe8c79181dc2",
+  databaseURL: "https://hackathon-2022-81f7b-default-rtdb.firebaseio.com",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
 
 let temp_live_data = document.getElementById("temp_live_data");
 let hum_live_data = document.getElementById("hum_live_data");
 let soil_live_data = document.getElementById("soil_live_data");
-let ttt3 = document.getElementById("temp_live_data1");
 
-function field1()
-{
-  setInterval(function(){
-    fetch('https://api.thingspeak.com/channels/1833491/fields/field1/last.txt?api_key=7D92ULX6128DMIYA')
-    .then((response) => response.json())
-    .then((data) => temp_live_data.innerHTML = ' '+data+' C');
-  },1000);
-}
+let avg_temp_val = document.getElementById("avg_temp_val");
+let avg_hum_val = document.getElementById("avg_hum_val");
+let avg_moisture_val = document.getElementById("avg_moisture_val");
 
-function field2()
-{
-  setInterval(function(){
-    fetch('https://api.thingspeak.com/channels/1833491/fields/field2/last.txt?api_key=7D92ULX6128DMIYA')
-    .then((response) => response.json())
-    .then((data) => hum_live_data.innerHTML = ' '+data+'%');
-  },1000);
+let dbref = ref(db, "Sensor_data");
+onValue(dbref, (snapshot) => {
+  let crops = [];
+  snapshot.forEach((childSnapshot) => {
+    crops.push(childSnapshot.val());
+    const data = snapshot.val();
+    console.log(data.Humidity);
+    temp_live_data.innerText = data.Temperature;
+    hum_live_data.innerText = data.Humidity;
+    soil_live_data.innerText = data.Moisture;
+    console.log(data);
 
-}
+    var display_sensor_1 = document.getElementById("display-sensor-1");
+    var value_1=display_sensor_1.options[display_sensor_1.selectedIndex].value;// get selected option value
+    var text_1=display_sensor_1.options[display_sensor_1.selectedIndex].innerText = "Average: "+data.Temperature;
 
-function field3()
-{
-  setInterval(function(){
-    fetch('https://api.thingspeak.com/channels/1833491/fields/field3/last.txt?api_key=7D92ULX6128DMIYA')
-    .then((response) => response.json())
-    .then((data) => soil_live_data.innerHTML = ' '+data);
-  },1000);
+    var display_sensor_2 = document.getElementById("display-sensor-2");
+    var value_2=display_sensor_2.options[display_sensor_2.selectedIndex].value;// get selected option value
+    var text_2=display_sensor_2.options[display_sensor_2.selectedIndex].innerText = "Average: "+data.Moisture;
 
-}
+    var display_sensor_3 = document.getElementById("display-sensor-3");
+    var value_3=display_sensor_3.options[display_sensor_3.selectedIndex].value;// get selected option value
+    var text_3=display_sensor_3.options[display_sensor_3.selectedIndex].innerText = "Average: "+data.Humidity;
 
-document.addEventListener('DOMContentLoaded',function(){
-  field1();
-  field2();
-  field3();
+  });
 });
+
