bddd2c21f26c6f456afb5729ee43c23d49ba88f8
mufoca <focahackathonteam@gmail.com>
2022-10-08 12:49:58 +0530

    init

--- js/script.js
+++ js/script.js
@@ -25,7 +25,7 @@ btn.addEventListener('click',()=>{
         status.style = "color:#000000";
         const reference = ref(db, 'Motor Status/');
         update(reference, {
-           motor_status:"ON"
+           motor_status:1.1
         });
         console.log("Your Motor is ON");
         sendNotification("Motor Status",`Motor is ON`,"","Motor Status");
@@ -36,7 +36,7 @@ btn.addEventListener('click',()=>{
         status.style = "color: #000000";
         const reference = ref(db, 'Motor Status/');
         update(reference, {
-           motor_status:"OFF"
+           motor_status:1.0
         });
         console.log("Your Motor is OFF");
         sendNotification("Motor Status",`Motor is OFF`,"","Motor Status");
@@ -48,13 +48,13 @@ setInterval(() => {
     {
         const reference = ref(db, 'Motor Status/');
         update(reference, {
-            motor_status:"ON"
+            motor_status:1.1
         });
     }
     else{
         const reference = ref(db, 'Motor Status/');
         update(reference, {
-            motor_status:"OFF"
+            motor_status:1.0
         });
     }
 }, 1000);
